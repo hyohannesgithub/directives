6d380bad0647cc4d7b938956011661ce269de449
hyohannesgithub <hyohannes@hotmail.com>
2017-09-03 14:57:27 +0100

    generic common angular todo files and folders added

---
+++ js/service.js
@@ -0,0 +1,10 @@
+	angular.module('TodoService', [])
+    .factory('TodoAPIService', function($http) {
+        TodoAPIService = {
+            getTodos: function(url, data, token) {
+                var header = "Authorization: JWT " + token;
+                return $http.get(url, {params:{"username": data}}, header);
+            }
+        };
+        return TodoAPIService;
+    });
